3ea7a53185ed64aa9d1371fdd17217b2bcc4c27b
Johnny Preyer <jpreyer@rsglab.com>
2017-06-15 11:18:45 -0400

    - Striping spaces from city and country - loading {county:city} into array - added breaking on empty enter

--- Exercise-01/hw1.py
+++ Exercise-01/hw1.py
@@ -3,11 +3,22 @@ import re
 pattern = "r'([\w\s]*),([\w\s]*)'"
 
 looping = True
-places = {}
+places_list=[]
+#places = {}
 while looping:
+    places = {}
     destination = raw_input("Tell me where you went: ")
-    m = re.search(r'([\w\s]*),([\w\s]*)',destination)
-    city = m.group(1)
-    country = m.group(2)
-    places[city] = country
-    print "CITY:"+city+"  COUNTRY:"+country
+    if destination == "":
+        break
+    else:
+        m = re.search(r'([\w\s]*),([\w\s]*)',destination)
+        city = m.group(1)
+        country = m.group(2)
+        m1 = re.search(r'\s*([\w\s]*)\s*', city)
+        m2 = re.search(r'\s*([\w\s]*)\s*', country)
+        city = m1.group(1)
+        country = m2.group(1)
+        places[country] = city
+        places_list.append(places)
+        #print "CITY:"+city+"  COUNTRY:"+country
+print places_list
